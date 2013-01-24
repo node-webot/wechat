6785868b4a5cc6e2e657c310a1e30a23f783fc6c
Jackson Tian <shyvo1987@gmail.com>
2013-01-25 02:24:01 +0800

    Update test/app.js

--- test/app.js
+++ test/app.js
@@ -5,7 +5,7 @@ var app = connect();
 app.use(connect.query());
 app.use('/wechat', wechat('some token', function (req, res, next) {
   var info = req.weixin;
-  res.reply({msgType: 'text', content: 'hehe'});
+  res.reply('hehe');
 }));
 
 module.exports = app;
