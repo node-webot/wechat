ffc05b7d7074992c8f3deebf5bd0a4a22d166cb8
Jackson Tian <shyvo1987@gmail.com>
2013-01-25 02:49:50 +0800

    Update test/app.js

--- test/app.js
+++ test/app.js
@@ -4,8 +4,22 @@ var wechat = require('../');
 var app = connect();
 app.use(connect.query());
 app.use('/wechat', wechat('some token', function (req, res, next) {
+  // 微信输入信息都在req.weixin上
   var info = req.weixin;
-  res.reply('hehe');
+  // 回复屌丝(普通回复)
+  if (info.FromUserName === 'diaosi') {
+    res.reply('hehe');
+  } else {
+  // 回复高富帅(图文回复)
+    res.reply([
+      {
+        title: '你来我家接我吧',
+        description: '这是女神与高富帅之间的对话',
+        picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
+        url: 'http://nodeapi.cloudfoundry.com/'
+      }
+    ]);
+  }
 }));
 
 module.exports = app;
