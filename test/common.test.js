98e6155cc7cd9851b5710ee18ebe3f133c79a3e8
Jackson Tian <shyvo1987@gmail.com>
2013-08-19 11:54:28 +0800

    添加菜单功能

--- test/common.test.js
+++ test/common.test.js
@@ -1,11 +1,11 @@
 var should = require('should');
 var urllib = require('urllib');
+var muk = require('muk');
 var API = require('../').API;
 
 describe('common.js', function () {
   describe('getAccessToken', function () {
     var api = new API('appid', 'secret');
-    var api2 = new API('appid', 'secret');
 
     it('should not ok', function (done) {
       api.getAccessToken(function (err, token) {
@@ -17,18 +17,16 @@ describe('common.js', function () {
     });
 
     describe('mock urllib err', function () {
-      var request;
       before(function () {
-        request = urllib.request;
-        urllib.request = function (url, args, callback) {
+        muk(urllib, 'request', function (url, args, callback) {
           var err = new Error('Urllib Error');
           err.name = 'UrllibError';
           callback(err);
-        };
+        });
       });
 
       after(function () {
-        urllib.request = request;
+        muk.restore();
       });
 
       it('should get mock error', function (done) {
@@ -36,6 +34,28 @@ describe('common.js', function () {
           should.exist(err);
           err.name.should.be.equal('WeChatAPIUrllibError');
           err.message.should.be.equal('Urllib Error');
+          done();
+        });
+      });
+    });
+
+    describe('mock token', function () {
+      before(function () {
+        muk(urllib, 'request', function (url, args, callback) {
+          process.nextTick(function () {
+            callback(null, {"access_token": "ACCESS_TOKEN","expires_in": 7200});
+          });
+        });
+      });
+      after(function () {
+        muk.restore();
+      });
+
+      it('should ok', function (done) {
+        api.getAccessToken(function (err, token) {
+          should.not.exist(err);
+          token.should.have.property('access_token', 'ACCESS_TOKEN');
+          token.should.have.property('expires_in', 7200);
           done();
         });
       });
@@ -51,7 +71,7 @@ describe('common.js', function () {
     });
 
     it('createMenu should not ok', function (done) {
-      api.createMenu('{}', function (err, token) {
+      api.createMenu('{}', function (err, menu) {
         should.exist(err);
         err.name.should.be.equal('WeChatAPIError');
         err.message.should.be.equal('invalid credential');
@@ -60,7 +80,7 @@ describe('common.js', function () {
     });
 
     it('getMenu should not ok', function (done) {
-      api.getMenu(function (err, token) {
+      api.getMenu(function (err, menu) {
         should.exist(err);
         err.name.should.be.equal('WeChatAPIError');
         err.message.should.be.equal('invalid credential');
@@ -80,13 +100,15 @@ describe('common.js', function () {
     it('getQRCode should not ok', function (done) {
       var ticket = {"expire_seconds": 1800, "action_name": "SCAN_SCENE", "action_info": {"scene": {"scene_id": 123}}};
       api.getQRCode(ticket, function (err, data, res) {
-        console.log(res.statusCode);
         should.exist(err);
-        console.log(data);
         err.name.should.be.equal('WeChatAPIError');
         err.message.should.be.equal('invalid credential');
         done();
       });
     });
+  });
+
+  describe('mock', function () {
+    
   });
 });
