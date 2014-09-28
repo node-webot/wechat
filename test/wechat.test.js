d75ab858e24bd06782c1230d87cc91ad1c33d4d8
ifeiteng <ifeiteng@gmail.com>
2014-09-28 12:18:30 +0800

    add test case

--- test/wechat.test.js
+++ test/wechat.test.js
@@ -32,6 +32,10 @@ app.use('/wechat', wechat('some token', function (req, res, next) {
     res.transfer2CustomerService();
   } else if (info.FromUserName === 'kf') {
     res.transfer2CustomerService('test1@test');
+  } else if (info.FromUserName === 'ls') {
+    res.reply(info.SendLocationInfo.EventKey);
+  } else if (info.FromUserName === 'pic_weixin') {
+    res.reply(info.SendPicsInfo.EventKey);
   } else {
   // 回复高富帅(图文回复)
     res.reply([
@@ -222,6 +226,61 @@ describe('wechat.js', function () {
         body.should.include('<Description><![CDATA[一无所有>]]></Description>');
         body.should.include('<MusicUrl><![CDATA[http://mp3.com/xx.mp3?a=b&c=d]]></MusicUrl>');
         body.should.include('<HQMusicUrl><![CDATA[http://mp3.com/xx.mp3?foo=bar]]></HQMusicUrl>');
+        done();
+      });
+    });
+
+    it('should ok with event location_select', function (done) {
+      var info = {
+        sp: 'nvshen',
+        user: 'ls',
+        type: 'event',
+        xPos: '80',
+        yPos: '70',
+        label: 'alibaba',
+        event: 'location_select',
+        eventKey: 'sendLocation',
+        text: '测试中'
+      };
+
+      request(app)
+      .post('/wechat' + tail())
+      .send(template(info))
+      .expect(200)
+      .end(function(err, res){
+        if (err) return done(err);
+        var body = res.text.toString();
+        body.should.include('<ToUserName><![CDATA[ls]]></ToUserName>');
+        body.should.include('<FromUserName><![CDATA[nvshen]]></FromUserName>');
+        body.should.match(/<CreateTime>\d{13}<\/CreateTime>/);
+        body.should.include('<MsgType><![CDATA[text]]></MsgType>');
+        body.should.include('<Content><![CDATA[sendLocation]]></Content>');
+        done();
+      });
+    });
+
+    it('should ok with event pic_weixin', function (done) {
+      var info = {
+        sp: 'nvshen',
+        user: 'pic_weixin',
+        type: 'event',
+        event: 'pic_weixin',
+        eventKey: 'sendPic',
+        text: '测试中'
+      };
+
+      request(app)
+      .post('/wechat' + tail())
+      .send(template(info))
+      .expect(200)
+      .end(function(err, res){
+        if (err) return done(err);
+        var body = res.text.toString();
+        body.should.include('<ToUserName><![CDATA[pic_weixin]]></ToUserName>');
+        body.should.include('<FromUserName><![CDATA[nvshen]]></FromUserName>');
+        body.should.match(/<CreateTime>\d{13}<\/CreateTime>/);
+        body.should.include('<MsgType><![CDATA[text]]></MsgType>');
+        body.should.include('<Content><![CDATA[sendPic]]></Content>');
         done();
       });
     });
