4a3d696a440724a525c88c8b93c1013c838f7abd
Jackson Tian <shyvo1987@gmail.com>
2014-10-24 12:08:40 +0800

    Bump 0.8.16

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var wechat = require('./lib/wechat');
 // 等待回复
 wechat.List = require('./lib/list');
 // 事件
-wechat.Event = require('./lib/event');
+wechat.Event = require('./lib/events');
 
 var API = require('./lib/api_common');
 // 菜单接口
