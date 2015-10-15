d088617462136265137abea08771dafa5ef17f08
Lames Zeng <lames.zeng@cargosmart.com>
2015-10-15 17:29:04 +0800

    wechat - List -  fixed testcase error - trouble shooting 500 error

--- test/list.test.js
+++ test/list.test.js
@@ -63,23 +63,4 @@ describe('list.js', function () {
     list.get('b').should.be.equal(common[1][1]);
     list.get('c').should.be.equal(common[2][1]);
   });
-
-  it('should ok when run in multiple nodejs instance', function() {
-    var common = [
-      ['选择{a}查看啥', function () {return 'aa'}],
-      ['选择{b}查看啥', function () {return 'bb'}],
-      ['选择{c}查看啥', function () {return 'cc'}]
-    ];
-    var head = '我是头';
-    var delimiter = '-------';
-    var foot = '我是小尾巴';
-    var req = {wxsession:{}};
-    List.add('common', common, head, delimiter, foot,req);
-    req.wxsession._list.should.not.be.empty;
-    var list = List.unserialize(req.wxsession._list);
-    list.description.should.be.equal(head + '\n选择a查看啥\n' + delimiter + '\n选择b查看啥\n' + delimiter + '\n选择c查看啥\n' + foot);
-    list.get('a')().should.be.equal(common[0][1]());
-    list.get('b')().should.be.equal(common[1][1]());
-    list.get('c')().should.be.equal(common[2][1]());
-  });
 });
